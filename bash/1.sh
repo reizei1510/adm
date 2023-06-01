da768729c3923007ed43c5e5c9c50c8adc836e05
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2023-06-02 02:39:55 +0300

    commit

--- bash/1.sh
+++ bash/1.sh
@@ -3,7 +3,7 @@ LOGFILE=$1
 TIME=$2
 while [[ $TIME > 0 ]]
 do
-echo $(date '+[%d.%m](http://%d.%m/).%y %H:%M') = $(</proc/loadavg) >> $LOGFILE
-TIME=$(($TIME-1))
-sleep 1 
+  echo $(date '+[%d.%m](http://%d.%m/).%y %H:%M') = $(</proc/loadavg) >> $LOGFILE
+  TIME=$(($TIME-1))
+  sleep 1 
 done
