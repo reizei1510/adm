0ee83bf30d95135c9a9c5f48a90789984ca68d44
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2023-06-02 01:43:00 +0300

    Create 2.sh

---
+++ bash/2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+PATH=$1
+if [ ! -d "$PATH" ]
+then
+    echo "$path does not exist"
+    exit
+fi
+
+SUBDIRS=$(echo $(basename -a $PATH/*/ | paste -d ' ' -s -))
+read -ra SUBDIRSLIST <<< "$SUBDIRS"
+for SUBDIR in "${SUBDIRSLIST[@]}"
+do
+    echo $(ls $PATH/$SUBDIR | wc -l) >> "$SUBDIR"
+done
