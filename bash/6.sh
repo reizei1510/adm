06c30a82ddf2255408756a7a704f5d1ae6d56487
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2023-06-02 02:38:55 +0300

    commit

--- bash/6.sh
+++ bash/6.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-if [ "$FOO" == "1" ]; then
+if [ "$FOO" == "1" ] || [ "$BAR" == "5" ]
+then
     exit
-if [ "$BAR" == "5" ]; then
-    exit
+fi
 DIR=$1
 while true
 do
-FILE=$(find "$DIR" -maxdepth 0)
-if [ -f "$FILE" ]
-then
-    echo "File $FILE created!"
-    exit
-fi
-sleep 1;
+    FILE=$(find "$DIR" -maxdepth 0)
+    if [ -f "$FILE" ]
+    then
+        echo "File $FILE created!"
+        exit
+    fi
+    sleep 1;
 done
