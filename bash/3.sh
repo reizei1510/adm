c539b3ed14241dea8d88b35396a31f230be11a07
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2023-06-02 02:39:31 +0300

    commit

--- bash/3.sh
+++ bash/3.sh
@@ -2,11 +2,11 @@
 COUNT=1
 for FILE in "$@"
 do
-if [ -f "$FILE" ]
-then
-    echo $(wc -l $FILE) 
-else
-    echo "$FILE does not exist"
-fi
-COUNT=$(( $COUNT + 1 ))
+    if [ -f "$FILE" ]
+    then
+        echo $(wc -l $FILE) 
+    else
+        echo "$FILE does not exist"
+    fi
+    COUNT=$(( $COUNT + 1 ))
 done
