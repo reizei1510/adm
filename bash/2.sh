9fc308a8495999768bf458eabd6dbc785c3f4d05
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2023-06-02 01:46:18 +0300

    commit

--- bash/2.sh
+++ bash/2.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-
 PATH=$1
-
 if [ ! -d "$PATH" ]
 then
     echo "$path does not exist"
     exit
 fi
-
 SUBDIRS=$(echo $(basename -a $PATH/*/ | paste -d ' ' -s -))
 read -ra SUBDIRSLIST <<< "$SUBDIRS"
 for SUBDIR in "${SUBDIRSLIST[@]}"
